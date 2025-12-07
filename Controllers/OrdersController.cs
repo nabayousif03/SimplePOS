82e02b2a39d4ab06f411682c9b12a476588505ba
nabayousif03 <bit23_1_50@uoitc.edu.iq>
2025-12-07 09:51:34 +0300

    ff

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -7,36 +7,31 @@ using System.Threading.Tasks;
 
 namespace SimplePOS.Controllers
 {
-    [Route("api/[controller]")] // This makes the URL: http://localhost:xxxx/api/orders
+    [Route("api/[controller]")]
     [ApiController]
     public class OrdersController : ControllerBase
     {
         private readonly DataContext _context;
-
-        // Constructor: This injects the database connection so we can use it
+        
         public OrdersController(DataContext context)
         {
             _context = context;
         }
-
-        // 1. GET: api/orders/pending
-        // The Kitchen uses this to see what to cook
+        
         [HttpGet("pending")]
         public async Task<ActionResult<IEnumerable<orders>>> GetPendingOrders()
         {
-            // Fetch only orders where Status is NOT 'completed'
+            
             var pendingOrders = await _context.Orders
                                       .Where(o => o.Status == "pending")
                                       .ToListAsync();
             return Ok(pendingOrders);
         }
-
-        // 2. POST: api/orders
-        // The Cashier uses this to submit a new order
+        
         [HttpPost]
         public async Task<ActionResult<orders>> CreateOrder(orders order)
         {
-            // Force these default values just in case
+            
             order.Status = "pending"; 
             order.OrderDate = DateTime.Now;
 
@@ -45,10 +40,7 @@ namespace SimplePOS.Controllers
 
             return CreatedAtAction(nameof(GetPendingOrders), new { id = order.Id }, order);
         }
-
-        // 3. PUT: api/orders/5/complete
-        // The Kitchen uses this to mark order #5 as Done
-        [HttpPut("{id}/complete")]
+        
         public async Task<IActionResult> MarkOrderComplete(int id)
         {
             var order = await _context.Orders.FindAsync(id);
