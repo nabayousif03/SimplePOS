ae4cbc695b8486838f4ecf4d26124cc07a8e08f9
nabayousif03 <bit23_1_50@uoitc.edu.iq>
2025-12-07 12:42:18 +0300

    j

---
+++ Data/OrdersForm.cs
@@ -0,0 +1,8 @@
+namespace SimplePOS.Data;
+
+public class OrdersForm
+{
+    public int totalPrice { get; set; } 
+    public string Status { get; set; }
+    public DateTime DeliveryDate { get; set; }
+}
